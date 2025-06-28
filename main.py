2e756041a5e5c90268a964a7f3f860ef392848a2
gaussuddin <114355359+gaussuddin@users.noreply.github.com>
2025-06-28 14:59:09 +0600

    Update main.py

--- main.py
+++ main.py
@@ -59,7 +59,7 @@ init_db()
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
 KEYWORDS = [
-  "নিয়োগ", "recruitment", "job", "career", "advertisement", "opportunity"
+  "চাকরি", "নিয়োগ", "recruitment", "job", "career", "advertisement", "opportunity"
 ]
 
 HEADERS = {
